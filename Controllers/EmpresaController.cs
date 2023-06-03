55b05e86ccffb3b7c65d1b1efd66433d4d852d01
jose martin sanabria flores <fjose6586@gmail.com>
2023-06-02 23:01:24 -0600

    postulacions y modificacion empresas

--- Controllers/EmpresaController.cs
+++ Controllers/EmpresaController.cs
@@ -1,9 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MiChamba.Data;
+using MiChamba.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace MiChamba.Controllers
 {
-    public class EmpresaController : Controller
+    public class EmpresaController : BaseController<EmpresaController>
     {
+        public EmpresaController(MiChambaDbContext db)
+        {
+            _db = db;
+
+        }
         public IActionResult Index()
         {
             return View();
@@ -12,6 +21,24 @@ namespace MiChamba.Controllers
        public IActionResult AdministrarPostulaciones()
         {
             return View();
+        }
+        public List<object> OfertasDisponibles()
+        {
+            var oferta = _db.Ofertas.FirstOrDefault(o => o.IdOferta == 1);
+
+            if (oferta != null)
+            {
+                // Acceder al JSON almacenado en la propiedad Requisitos
+                var requisitosJson = oferta.Requisitos;
+
+                // Aquí puedes realizar las operaciones necesarias con el JSON, como parsearlo o utilizarlo directamente
+                // Por ejemplo, puedes convertirlo a un objeto o mostrarlo en tu vista
+
+                return oferta;
+            }
+
+
+            return null;
         }
     }
 }
